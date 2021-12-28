8c6c97bb0f9157a605bd743777949a7c7f35baa6
dhanraju <dhanunjay.raju@gmail.com>
2021-12-27 21:58:27 -0800

    git push issue fix.

--- number_system_xmlrunner.py
+++ number_system_xmlrunner.py
@@ -12,10 +12,5 @@ def find_tests():
 	return unittest.TestLoader().discover('maths', pattern='*_test.py')
 
 if __name__ == '__main__':
-<<<<<<< HEAD
 	print("start find_tests")
 	runner().run(find_tests())
-=======
-	print 'start find_tests'
-	runner().run(find_tests())
->>>>>>> 571e8f3b67ea37923b9b6d927446d86c68552482
