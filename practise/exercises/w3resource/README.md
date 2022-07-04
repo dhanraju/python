185259e16bacfd0c369d620cf1971c0e587770d5
dhanraju <dhanunjay.raju@gmail.com>
2022-07-04 10:10:55 -0700

    w3resource exercise.

---
+++ practise/exercises/w3resource/README.md
@@ -0,0 +1,3 @@
+Ref link for python exercises in w3resource:
+
+https://www.w3resource.com/python-exercises/
---
+++ practise/exercises/w3resource/ex01_basics/ex01.py
@@ -0,0 +1,25 @@
+"""
+1. Write a Python program to print the following string in a specific format
+(see the output).
+Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up\
+above the world so high, Like a diamond in the sky. Twinkle, twinkle, little\
+star, How I wonder what you are"
+
+Output :
+Twinkle, twinkle, little star,
+        How I wonder what you are! 
+                Up above the world so high,   		
+                Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+        How I wonder what you are
+"""
+
+def prog001(input_string):
+    # Convert input string to the given format.
+    # Algorithm:
+    # * Split the string into new lines when capital letters are found.
+    # * Exclude I while splitting the string to lines.
+    # * Add 8 spaces at the beginning of the new line.
+    # * Add 16 spaces at the beginning of the new line from line 3 to remaining
+    #   lines.
+    # * No spaces added to the line starts with Twinkle.
