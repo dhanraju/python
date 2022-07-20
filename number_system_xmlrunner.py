dcce2ed7ab43ea72e4d46f5af8d80163ae74d163
dhanraju <dhanunjay.raju@gmail.com>
2022-07-19 20:55:23 -0700

    First step to support more unit tests for xmlrunner.

--- number_system_xmlrunner.py
+++ number_system_xmlrunner.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import os
 import unittest
 import xmlrunner
 
@@ -10,8 +11,12 @@ def runner(output="test-reports"):
 
 def find_tests():
 	print("In find_tests.")
-	return unittest.TestLoader().discover("maths", pattern="*_test.py")
+	folders_list = []
+	# folders_list = os.path.dirname(os.path.abspath(__file__))
+	folders_list = os.path.dirname(__file__)
+	return unittest.TestLoader().discover(folders_list, pattern="*_test.py")
 
 if __name__ == '__main__':
+	# Make sure the file __init__.py exists in every directory that have tests.
 	print("start find_tests")
 	runner().run(find_tests())
