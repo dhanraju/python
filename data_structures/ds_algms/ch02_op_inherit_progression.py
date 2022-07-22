c2b33727b75e91cb5fb6f374113e3f7d7b346f7b
dhanraju <dhanunjay.raju@gmail.com>
2022-07-22 12:51:34 -0700

    ch02_oops file rename

---
+++ data_structures/ds_algms/ch02_op_inherit_progression.py
@@ -0,0 +1,7 @@
+"""Progression that demonstrates Inheritance concept."""
+
+class Progression:
+    """Iterator producing a generic progression.
+
+    Default iterator produces the whole numbers 0, 1, 2, ...
+    """
