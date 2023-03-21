31667386b7a58ffe7ccd7a0734477218c46b8562
dhanraju <dhanunjay.raju@gmail.com>
2023-03-20 22:26:35 -0700

    pylint command added to the script.

--- run_tests_with_lint.sh
+++ run_tests_with_lint.sh
@@ -10,14 +10,16 @@ virtualenv testenv -p /usr/bin/python3
 set -e
 set -x
 
-rm -f pep8.log pyflakes.log
+# rm -f pep8.log pyflakes.log
 
-./number_system_xmlrunner.py
+# ./number_system_xmlrunner.py
 
 # Install pep8 tool.
-# $ sudo apt install pep8
-pep8 --max-line-length=80 ./ > pep8.log || true
+# #$ sudo apt install pep8
+# pep8 --max-line-length=80 ./ > pep8.log || true
 
 # Install pyflakes tool.
-# $ sudo pip install --upgrade pyflakes
-pyflakes ./ > pyflakes.log || true
+# #$ sudo pip install --upgrade pyflakes
+# pyflakes ./ > pyflakes.log || true
+
+pylint --output-format=parseable --fail-under=9.5 --rcfile=pylint.cfg $(find . -maxdepth 5 -name "*.py" -print) --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" | tee pylint.log || echo "pylint exited with $?"' echo "Linting Success, Generating Report" recordIssues enabledForFailure: true, aggregatingResults: true, tool: pyLint(pattern: 'pylint.log')'
